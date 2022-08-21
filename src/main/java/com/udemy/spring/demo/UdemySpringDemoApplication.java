485645bddf65af11f3c0e382b4318d2c9bf72bf1
ir96 <inreshpatel96@gmail.com>
2022-08-22 02:27:42 +0530

    IR | spring bean(applicationContext)

---
+++ src/main/java/com/udemy/spring/demo/UdemySpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringDemoApplication.class, args);
+	}
+
+}
