1240034d8116724862c179629475a6643a51ad0f
ir96 <inreshpatel96@gmail.com>
2022-09-06 23:50:37 +0530

    IR | setter injection

--- src/main/java/com/udemy/spring/demo/CricketCoach.java
+++ src/main/java/com/udemy/spring/demo/CricketCoach.java
@@ -1,16 +1,33 @@
 package com.udemy.spring.demo;
 
-public class CricketCoach implements Coach{
+public class   CricketCoach implements Coach{
 
     //define a private field for the dependency
     private FortuneService fortuneService;
 
-    //define a constructor for dependency injection
+    //define a constructor for dependency injection(no-org constructor)
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside setter method - default");
+    }
+
     public CricketCoach(FortuneService theFortuneService) {
         fortuneService = theFortuneService;
+        System.out.println("CricketCoach: inside setter method");
     }
+
+    @Override
     public String getDailyWorkout() {
         return "play for 50 overs";
+    }
+
+    public FortuneService getFortuneService() {
+        return fortuneService;
+    }
+
+    //setter method
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method- setFortuneService");
+        this.fortuneService = fortuneService;
     }
 
     @Override
