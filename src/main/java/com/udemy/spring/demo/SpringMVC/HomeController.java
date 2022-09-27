03a0a2c7564efc771be473a308b53bec00c47915
ir96 <inreshpatel96@gmail.com>
2022-09-27 23:09:39 +0530

    IR | spring MVC setup

---
+++ src/main/java/com/udemy/spring/demo/SpringMVC/HomeController.java
@@ -0,0 +1,16 @@
+package com.udemy.spring.demo.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/hello")
+    public String showPage() {
+        return "menu";
+    }
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/view/menu.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Spring MVC</title>
+</head>
+<body>
+    <h2>Spring MVC - Home Page</h2>
+</body>
+</html>
